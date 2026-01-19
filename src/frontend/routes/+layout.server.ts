31a89430e39b07e86dd1e5f4947588b04447d0e2
Migru User <user@migru.app>
2026-01-19 13:21:13 +0000

    fix(build): disable prerendering to fix auth error during build

--- src/frontend/routes/+layout.server.ts
+++ src/frontend/routes/+layout.server.ts
@@ -1,6 +1,8 @@
 import type { LayoutServerLoad } from './$types';
 import { buildClerkProps } from 'svelte-clerk/server';
 
+export const prerender = false;
+
 export const load: LayoutServerLoad = async ({ locals }) => {
 	return {
 		...buildClerkProps(locals.auth)
